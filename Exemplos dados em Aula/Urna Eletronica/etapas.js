7a303456d1abd97aac6fcff4010b937c7d1eda3e
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-10-16 20:45:17 -0300

    Começou a mexer no JS
    Começamos criando o etapas.js que é a cheio de variavel para preenchimento automatico do nosso candidato, e no script.js vamos criar varias variavel pra começa a pega o controle de interface, e as funcions para cada um dos nosso botoes

--- Exemplos dados em Aula/Urna Eletronica/etapas.js
+++ Exemplos dados em Aula/Urna Eletronica/etapas.js
@@ -1,12 +1,56 @@
 let etapas = [
     {
-        titulo: 'VEREADOR',
+        titulo: 'PRESIDENTE',
+        numeros:2,
+        candidatos:[
+            {
+                numero:13,
+                nome:'Lula',
+                partido:'PT',
+                fotos:[
+                    {url:'cadidato1.jpg',legenda:'Presidente',small:false}]
+
+            },
+            {
+                numero:45,
+                nome:'Geraldo Alkimin',
+                partido:'PSDB',
+                fotos:[
+                    {url:'deputado.jpg',legenda:'Presidente'}]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
         numeros:5,
         candidatos:[
             {
-                numero:38111,
-                nome:''
+                numero: 13313,
+                nome:'Haddad',
+                partido:'PT',
+                vice:'Dilma',
+                fotos:[
+                    {url:'cadidato3.jpg',legenda: 'Prefeito'},
+                    {url:'senador.jpg',legenda:'Vice-Prefeito',small: true}
+                ]
+
+            },
+            {
+                numero:45545,
+                nome:'Kassab',
+                partido:'PSDB',
+                vice:'Doria',
+                fotos:[
+                    {url:'vereador.jpg',legenda:'Prefeito'},
+                    {url:'cadidato4.jpg',legenda:'Vice-Prefeito',small:true}
+
+                ]
             }
+
         ]
     }
+
+
+
+
 ]
---
+++ Exemplos dados em Aula/Urna Eletronica/script.js
@@ -0,0 +1,27 @@
+//CONTROLE DE INTERFACE
+var seuVotoPara = document.querySelector('.d-1-1 span')
+var cargo = document.querySelector('.d-1-2 span')
+var descricao = document.querySelector('.d-1-4')
+var aviso = document.querySelector('.d-2')
+var lateral = document.querySelector('.d-1-rigth')
+var numeros = document.querySelector('.d-1-3')
+
+//CONTROLE DE AMBIENTE
+let 
+
+//FUNCTIONS 
+function clicou(n){
+    alert('clicou em ' + n)
+}
+function branco(){
+    alert('clicou em branco')
+
+}
+function corrigir(){
+    alert('clicou em corrige')
+
+}
+function confirmar(){
+    alert('clicou em confrimar')
+
+}
