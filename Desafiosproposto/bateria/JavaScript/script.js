277935b288e3a39dec7f53a4150ed3ec1345588e
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-10-27 22:05:18 -0300

    Atualizacao no HTML e adicionando evento de click e forEach
    Meu código para a bateria, funciona da seguinte maneira: primeiro eu pego o código o selector all as valiras do num, depois vamos fazer um for each onde colocamos a marca e indice, no  caso esses nomes eu ainda vou altera, e depois crio um switch para fazer a comparação, já testei e ate o dado momento esta funcionando

---
+++ Desafiosproposto/bateria/JavaScript/script.js
@@ -0,0 +1,47 @@
+/*
+
+1-) Colocar o item teclado--num em um array que seja funcional, para fazer alterações ----- OK  
+2-) Vamos pega esse item e adicionar uma função de click ---- OK
+3-) Depois preciso colocar uma funçao que pegue o data--num 
+4-) apos pega o data num, vamos adicionar um SWHIT 
+5-) Apos adiciona o swhift temos que fazer a comparação de valor clicado com o valor do case
+6-) Adiciona um função de retorno com cada toque para cada valor selecionado
+
+
+
+*/
+let clickNum = document.querySelectorAll('.teclado--num')
+let numarray = []
+
+clickNum.forEach((marca,indice) =>{
+    marca.addEventListener('click',()=>{
+        switch (indice){
+            case 0:
+                
+            break
+            case 1:
+                
+            break
+            case 2:
+
+            break
+            case 3:
+
+            break
+            case 4:
+
+            break
+            case 5:
+
+            break
+            case 6:
+
+            break
+            case 8:
+                
+            break
+        }
+
+    })
+})
+
