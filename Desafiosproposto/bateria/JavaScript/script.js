3b80bf6f602ae3c4c6b1d09512880f31114b66c7
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-10-27 22:15:37 -0300

    peguei o data-num
    consegui pega o data num, vamos agora coloca a musica pra tocar

--- Desafiosproposto/bateria/JavaScript/script.js
+++ Desafiosproposto/bateria/JavaScript/script.js
@@ -1,20 +1,21 @@
 /*
 
-1-) Colocar o item teclado--num em um array que seja funcional, para fazer alterações ----- OK  
-2-) Vamos pega esse item e adicionar uma função de click ---- OK
-3-) Depois preciso colocar uma funçao que pegue o data--num 
-4-) apos pega o data num, vamos adicionar um SWHIT 
-5-) Apos adiciona o swhift temos que fazer a comparação de valor clicado com o valor do case
+1-) Colocar o item teclado--num em um array que seja funcional, para fazer alterações ------ OK------
+2-) Vamos pega esse item e adicionar uma função de click ------ OK------
+3-) apos pega o data num, vamos adicionar um SWHIT ------ OK------
+4-) Depois preciso colocar uma funçao que pegue o data--num ------ OK------
+5-) Apos adiciona o swhift temos que fazer a comparação de valor clicado com o valor do case ------ OK------
 6-) Adiciona um função de retorno com cada toque para cada valor selecionado
 
 
 
 */
 let clickNum = document.querySelectorAll('.teclado--num')
-let numarray = []
+let numarray = 0
 
 clickNum.forEach((marca,indice) =>{
     marca.addEventListener('click',()=>{
+        numarray = marca.getAttribute('data-num')
         switch (indice){
             case 0:
                 
@@ -41,6 +42,8 @@ clickNum.forEach((marca,indice) =>{
                 
             break
         }
+
+
 
     })
 })
