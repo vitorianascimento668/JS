1baf8829d60a7200ea456ecc77aebc7484d5f4c4
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-11-13 22:45:36 -0300

    Ja converti os valores para a temperatura
    fiz primeiro o fazerconversao() para converte a lat e lon que eu peguei do site e pega os valores usando o map, depois passei esses valores para o PegaTemperatura() e passei 2 parametro que esta sendo usado para pega a temperatura dentro da url2 .
    
    Agora somente falta subir essas temperaturas para o meu html e converte elas para graus ₢

--- Desafiosproposto/clima/js/javascript.js
+++ Desafiosproposto/clima/js/javascript.js
@@ -1,6 +1,27 @@
 let botao = document.querySelector('.botao_busca')
-function clicou(){
-    let input = document.querySelector('input').value
 
-    console.log(input)
+function fazerConversao (dadosdaURL) {
+     dadosdaURL.map((parametro)=>{
+        let lat = parametro.lat
+        let lon = parametro.lon
+       pegarTemperatura(lat,lon)
+
+
+    })
 }
+
+ var clicou = async () =>{
+    let input = document.querySelector('input').value
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=297c8e1c49b86f7d3cd75ffb4de43966
+    `
+    let dados_url = await fetch(url) 
+    let dados_url_json = await dados_url.json()
+    fazerConversao(dados_url_json)
+
+   
+}
+function pegarTemperatura(lat,lon){
+    let url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=297c8e1c49b86f7d3cd75ffb4de43966   `
+    console.log(url2)
+}
+botao.addEventListener('click',clicou)
