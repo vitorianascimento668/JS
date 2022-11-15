bcaf29023de7a5592ce7820dfc1ba63dd1a3c04a
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-11-14 21:31:08 -0300

    Criei 2 variais, alterei a posição dos dados
    criei duas variaveis para corrigir o problema com os nomes das cidades, e nela estou passando os parametros nome e pais. Depois passei esses valores para meu innerhtml.
    
    Agora vou corrigir, para enquanto espera, aparece uma mensagem na tela, escrita aguarde

--- Desafiosproposto/clima/js/javascript.js
+++ Desafiosproposto/clima/js/javascript.js
@@ -1,26 +1,34 @@
 let botao = document.querySelector('.botao_busca')
 let conversaoKevin = 0
+let nome = ''
+let pais = ''
 
+var clicou = async () =>{
+    document.querySelector('.contaier-resposta').style.display = 'block'
+
+    let input = document.querySelector('input').value
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=297c8e1c49b86f7d3cd75ffb4de43966
+    `
+    let dados_url = await fetch(url) 
+    let dados_url_json = await dados_url.json()
+    console.log(dados_url_json , 'dados da url_json')
+    fazerConversao(dados_url_json)
+
+   
+}
 function fazerConversao (dadosdaURL) {
      dadosdaURL.map((parametro)=>{
         let lat = parametro.lat
         let lon = parametro.lon
+        nome = parametro.name 
+        pais = parametro.country
        pegarTemperatura(lat,lon)
 
 
     })
 }
 
- var clicou = async () =>{
-    let input = document.querySelector('input').value
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=297c8e1c49b86f7d3cd75ffb4de43966
-    `
-    let dados_url = await fetch(url) 
-    let dados_url_json = await dados_url.json()
-    fazerConversao(dados_url_json)
 
-   
-}
 var pegarTemperatura = async (lat,lon)=>{
     let url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=297c8e1c49b86f7d3cd75ffb4de43966   `
     var r = await fetch(url2)
@@ -30,12 +38,12 @@ var pegarTemperatura = async (lat,lon)=>{
 
 let mostrarTemperatura = async (res) =>{
         conversaoKevin = res.main.temp - 273.15
-        document.querySelector('.nome-estado').innerHTML = `${res.name} - ${res.sys.country}`
-        document.querySelector('.valor-vento').innerHTML = `${res.wind.speed}`
-        document.querySelector('.valor-temperatura').innerHTML = `${conversaoKevin.toFixed(2)}`
+        document.querySelector('.nome-estado').innerHTML = `${nome} - ${pais}`
+        document.querySelector('.valor-vento').innerHTML = `${res.wind.speed}<small> km/h</small>`
+        document.querySelector('.valor-temperatura').innerHTML = `${conversaoKevin.toFixed(2)} <small>°C</small>`
 
-
-        console.log(res)
+        
+        console.log(res ,'res')
     
 }
 botao.addEventListener('click',clicou)
