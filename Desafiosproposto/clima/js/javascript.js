cad4754b6feabdbe4918b8420ed8603dba2ef11a
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-11-14 22:50:01 -0300

    Coloquei a função erro
    Criei a função erro que sera chamada caso o valor de dados_url_json.length == 0, ele chama a função erro e aparece a mensagem de erro, quando o usario resolve, ele remove a mensagem e atualiza a pagina.

--- Desafiosproposto/clima/js/javascript.js
+++ Desafiosproposto/clima/js/javascript.js
@@ -3,28 +3,39 @@ let aguarde = document.querySelector('.aguarde')
 let container = document.querySelector('.contaier-resposta')
 let conversaoKevin = 0
 let quantidade = 0
+let res = []
+let temalgo = []
 let nome = ''
 let pais = ''
 
 
 var clicou = async () =>{
     quantidade ++
+    document.querySelector('.error').style.display = 'none'
+
    if (quantidade == 1){
     aguarde.style.display = 'block'
-    container.style.display = 'block'
     setTimeout(()=>{
         aguarde.style.display = 'none'
 
     },'500')    
    }
+   container.style.display = 'block'
 
     let input = document.querySelector('input').value
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=297c8e1c49b86f7d3cd75ffb4de43966
     `
     let dados_url = await fetch(url) 
     let dados_url_json = await dados_url.json()
-    console.log(dados_url_json , 'dados da url_json')
-    fazerConversao(dados_url_json)
+        if(dados_url_json.length == 0){
+            error()
+        }
+        else{
+           
+            fazerConversao(dados_url_json) }        
+        
+    
+
 
    
 }
@@ -32,6 +43,7 @@ function fazerConversao (dadosdaURL) {
      dadosdaURL.map((parametro)=>{
         let lat = parametro.lat
         let lon = parametro.lon
+
         nome = parametro.name 
         pais = parametro.country
        pegarTemperatura(lat,lon)
@@ -42,10 +54,13 @@ function fazerConversao (dadosdaURL) {
 
 
 var pegarTemperatura = async (lat,lon)=>{
+   
     let url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=297c8e1c49b86f7d3cd75ffb4de43966   `
     var r = await fetch(url2)
     var resultado = await r.json()
-    mostrarTemperatura(resultado)
+   
+        mostrarTemperatura(resultado)
+    
 }
 
 let mostrarTemperatura = async (res) =>{
@@ -53,10 +68,16 @@ let mostrarTemperatura = async (res) =>{
         document.querySelector('.nome-estado').innerHTML = `${nome} - ${pais}`
         document.querySelector('.valor-vento').innerHTML = `${res.wind.speed}<small> km/h</small>`
         document.querySelector('.valor-temperatura').innerHTML = `${conversaoKevin.toFixed(2)} <small>°C</small>`
-
+        idCidade = res.sys.id
+        id = res.id
         
         console.log(res ,'res')
     
+}
+function error (){
+    document.querySelector('.error').style.display = 'block'
+    container.style.display = 'none'
+   
 }
 botao.addEventListener('click',clicou)
 
