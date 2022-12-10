16703b7bcff69d29d87076f3d988a143803cdfea
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-10 17:54:04 -0300

    Terminei o relogio no javascript
    Comecei criando as variaveis auxilares para fazer  a transição, depois criei uma a funcção relogio analogico e ja chamei ela com o setInterval de 1 em 1 segundo, apois isso foi somente fazer a transição dos valores com matematica basica, para fazer os valores de minutos/segundos e horas fazer a transição para 360 minutos

--- Desafiosproposto/relogio/js/javas.js
+++ Desafiosproposto/relogio/js/javas.js
@@ -1,7 +1,12 @@
+let ponteiroSeg = document.querySelector('.ponteiro--segundos')
+let ponteiroMinutos = document.querySelector('.ponteiro--minutos')
+let ponteiroHoras = document.querySelector('.ponteiro--horas')
 var data = ''
 var horas = ''
 var minutos = ''
 var segundos = ''
+
+
 function atualizaRelogio(){
      data = new Date
      horas = data.getHours()
@@ -12,5 +17,14 @@ function relogioDigital(){
     atualizaRelogio()
     document.querySelector('.numeros-digital').innerHTML = `${horas} : ${minutos} : ${segundos}`
 }
+function relogioAnalogico (){
+    ponteiroSeg.style.transform = `translateY(-50%) rotate(${segundos*6}deg)`
+    ponteiroMinutos.style.transform = `translateY(-50%) rotate(${minutos * 6}deg)`
+    ponteiroHoras.style.transform = `translateY(-50%) rotate(${horas * 30}deg)`
+
+}
 setInterval(atualizaRelogio, '100')
 setInterval(relogioDigital, '100')
+setInterval(relogioAnalogico, '100')
+
+
