08b09ccf00d9b0dca26bc0f8effafe478cdd0884
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-14 17:40:20 -0300

    Comecei a criar  o javascript
    comecei criando duas variaveis, a do jogador1 e jogador2 onde cada uma vai atribui x ou o, depois criei a função jogo, e nela vou começa a fazer o jogo em si

---
+++ Desafiosproposto/jogodaVelha/js/javascript.js
@@ -0,0 +1,20 @@
+let jogador1 = 'X'
+let jogador2 = 'O'
+let tabuleiro_marc = document.querySelectorAll('.tabuleiro_marc')
+
+function jogo(){
+    
+}
+
+/*
+1-)verifica quem está jogando, jogador 1 ou jogador2
+2-)Atribuir o valor na tela e deixa-lo guardado em alguma var
+3-)
+4-) 
+5-)
+6-)
+7-)
+8-)
+9-)
+10-)
+*/
